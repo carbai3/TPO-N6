ad9b47c22126eca4863847166e2f944c26e9d429
carbai3 <163202615+carbai3@users.noreply.github.com>
2024-04-10 22:29:22 -0300

    Initial commit

---
+++ src/Entidades/Producto.java
@@ -0,0 +1,63 @@
+
+package Entidades;
+
+public class Producto {
+    private int codigo;
+    private String descripcion;
+    private Double precio;
+    private int stock;
+    private String rubro;
+
+    public Producto() {
+    }
+
+    public Producto(int codigo, String descripcion, Double precio, int stock, String rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.rubro = rubro;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+    
+    
+}
